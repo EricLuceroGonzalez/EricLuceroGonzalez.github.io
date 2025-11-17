105ddd9ea7cef99d1298671878be5f8e60dfa62f
Ericlucerogonzalez <35483789+EricLuceroGonzalez@users.noreply.github.com>
2025-11-17 23:57:09 +0100

    Creacion de otro mdx (entrada al blog) y ajuste del h2 header

--- app/components/MdCompos/H2Header.js
+++ app/components/MdCompos/H2Header.js
@@ -1,16 +1,36 @@
 import { MdSubHeadA } from "@/app/ui/MarkDownComponents";
 import { FaLink } from "react-icons/fa";
-
-const getAnchor = (anchorText) => {
-  return anchorText
-    .normalize("NFD") // separa los acentos de las letras
-    .replace(/[\u0300-\u036f]/g, "") // elimina los acentos
+const extractText = (node) => {
+  if (typeof node === "string") return node;
+  if (typeof node === "number") return String(node);
+  if (Array.isArray(node)) return node.map(extractText).join("");
+  if (typeof node === "object" && node?.props?.children) {
+    return extractText(node.props.children);
+  }
+  return "";
+};
+// const getAnchor = (anchorText) => {
+//   return (
+//     anchorText
+//       // .normalize("NFD") // separa los acentos de las letras
+//       .replace(/[\u0300-\u036f]/g, "") // elimina los acentos
+//       .toLowerCase()
+//       .replace(/[^a-z0-9]/g, "")
+//       .replace(/[]/g, "-")
+//   );
+// };
+const getAnchor = (text) => {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase()
-    .replace(/[^a-z0-9]/g, "")
-    .replace(/[]/g, "-");
+    .replace(/[^a-z0-9]/g, "-") // Recomiendo usar guiones en lugar de nada para separar palabras
+    .replace(/^-+|-+$/g, ""); // Elimina guiones al inicio o final
 };
 const H2Header = ({ children }) => {
-  const anchor = getAnchor(children[1]);
+  const anchorText = extractText(children);
+  const anchor = getAnchor(anchorText);
+  // const anchor = getAnchor(children[1]);
   const link = `#${anchor}`;
   console.log(children[1]);
   console.log(link);
